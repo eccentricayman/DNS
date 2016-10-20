4da8e90b686b28ef96a4569c11170f4ef067ec1a
jchua1 <jchua@stuy.edu>
2016-10-19 20:10:24 -0700

    fixed all song functions except free_list

--- songs.c
+++ songs.c
@@ -4,118 +4,136 @@
 #include <time.h>
 #include "songs.h"
 
-struct song_node * insert_front(struct song_node * start, char newSong[], char newArtist[]) {
-    struct song_node *toAdd = (struct song_node *)malloc(sizeof(struct song_node));
-    strcmp(toAdd -> name, newSong);
-    strcmp(toAdd -> artist, newArtist);
-    toAdd -> next = start;
-    return toAdd;
+song_node * insert_front(song_node * start, char newSong[], char newArtist[]) {
+  song_node *toAdd = (song_node *)malloc(sizeof(song_node));
+  strcpy(toAdd->name, newSong);
+  strcpy(toAdd->artist, newArtist);
+  toAdd->next = start;
+  return toAdd;
 }
 
-struct song_node * insert_order(struct song_node * start, char newSong[], char newArtist[]) {
-    if (!start || strcmp(start -> artist, newArtist)) {
-        return insert_front(start, newSong, newArtist);
+song_node * insert_order(song_node * start, char newSong[], char newArtist[]) {
+  if (!start || strcmp(start->artist, newArtist) > 0) {
+    return insert_front(start, newSong, newArtist);
+  }
+  song_node * beginning = start;
+  while (start->next) {
+    if (strcmp(start->next->artist, newArtist) < 0) {
+      start = start->next;
     }
-    song_node * beginning = start;
-    while (start -> next) {
-        if (strcmp(start -> next -> artist, newArtist)) {
-            start = start -> next;
-        }
-        else {
-            break;
-        }
-    }
-    song_node * toAdd = insert_front(start -> next, newSong, newArtist);
-    start -> next = toAdd;
-    return beginning;
-}
-
-void print_list(struct song_node * start) {
-    printf("%s by %s \n", start -> name, start -> artist);
-    if (start -> next) {
-        print_list(start -> next);
+    else if (strcmp(start->next->artist, newArtist) == 0) {
+      if (strcmp(start->next->name, newSong) < 0) {
+	start = start->next;
+      }
+      else {
+	break;
+      }
     }
     else {
-        printf("\n");
+      break;
     }
+  }
+  song_node * toAdd = insert_front(start->next, newSong, newArtist);
+  start->next = toAdd;
+  return beginning;
 }
 
-struct song_node * find_song(struct song_node * start, char songName[]) {
-    while (start) {
-        if (strcmp(start -> name, songName)) {
-            return start;
-        }
-        else {
-            start = start -> next;
-        }
-    }
-    return NULL;
+void print_list(song_node * start) {
+  printf("%s by %s \n", start->name, start->artist);
+  if (start->next) {
+    print_list(start->next);
+  }
+  else {
+    printf("\n");
+  }
 }
 
-struct song_node * first_artist_song(struct song_node * start, char artistName[]) {
-    while (start) {
-        if (strcmp(start -> artist, artistName)) {
-            break;
-        }
-        start = start -> next;
+song_node * find_song(song_node * start, char songName[]) {
+  while (start) {
+    if (!strcmp(start->name, songName)) {
+      return start;
     }
-    return start;
+    start = start->next;
+  }
+  return NULL;
 }
 
-struct song_node * random_element(struct song_node * start) {
-    srand(time(NULL));
-    int ctr = rand() % 2;
-    for (int i = 0 ; i <= ctr ; i++) {
-        start = start -> next;
+song_node * first_artist_song(song_node * start, char artistName[]) {
+  while (start) {
+    if (!strcmp(start->artist, artistName)) {
+      return start;
     }
-    return start;
+    start = start->next;
+  }
+  return NULL;
 }
 
-void remove_song(struct song_node * start, char songName[], char artistName[]) {
-    while (start -> next) {
-        if (strcmp(start -> next -> name, songName) && strcmp(start -> next -> name, artistName)) {
-            struct song_node * temp = start -> next;
-            start -> next = start -> next -> next;
-            free(temp);
-            break;
-        }
-    }
+int length(song_node * start) {
+  int ctr = 0;
+  while (start) {
+    ctr++;
+    start = start->next;
+  }
+  return ctr;
+}
+  
+song_node * random_element(song_node * start) {
+  srand(time(NULL));
+  int ctr = rand() % length(start);
+  for (int i = 0 ; i < ctr ; i++) {
+    start = start->next;
+  }
+  return start;
 }
 
-struct song_node * free_list(struct song_node * start) {
-    if (start -> next) {
-        free_list(start -> next);
+void remove_song(song_node * start, char songName[], char artistName[]) {
+  while (start->next) {
+    if (!strcmp(start->next->name, songName) && !strcmp(start->next->artist, artistName)) {
+      song_node * temp = start->next;
+      start->next = start->next->next;
+      free(temp);
+      break;
     }
-    free(start);
-    return start;
+    start = start->next;
+  }
+}
+
+song_node * free_list(song_node * start) {
+  song_node * temp = start; //SEG FAULT
+  while (start) {
+    start = start->next;
+    free(temp);
+    temp = start;
+  }
+  return start;
 }
 
 int main() {
-    //stolen from calvin and giovanni
-    song_node *list = insert_order(NULL, "Highway to Hell", "AC/DC");
-    list = insert_order(list, "Everlong", "Foo Fighters");
-    list = insert_order(list, "Hips Don't Lie", "Shakira");
-    list = insert_order(list, "Something Good Can Work", "Two Door Cinema Club");
-    list = insert_order(list, "American Idiot", "Green Day");
+  //stolen from calvin and giovanni
+  song_node *list = insert_order(NULL, "Highway to Hell", "AC/DC");
+  list = insert_order(list, "Everlong", "Foo Fighters");
+  list = insert_order(list, "Hips Don't Lie", "Shakira");
+  list = insert_order(list, "Something Good Can Work", "Two Door Cinema Club");
+  list = insert_order(list, "American Idiot", "Green Day");
   
-    print_list(list);
-    printf("Finding Everlong: %s\n", find_song(list, "Everlong")->name);
-    printf("Finding first name by foo fighters: %s\n", first_artist_song(list, "Foo Fighters")->name);
-    printf("Getting random name: %s\n", random_element(list)->name);
-    printf("Removing 'Hips Don't Lie'...\n");
-    remove_song(list, "Hips Don't Lie", "Shakira");
-    printf("\nRemoving 'American Idiot'...\n");
-    remove_song(list, "American Idiot", "Green Day");
-    printf("\nRemoving 'Something Good Can Work'...\n");
-    remove_song(list, "Something Good Can Work", "Two Door Cinema Club");
-    print_list(list);
+  print_list(list);
+  printf("Finding Everlong: %s\n", find_song(list, "Everlong")->name);
+  printf("Finding first name by foo fighters: %s\n", first_artist_song(list, "Foo Fighters")->name);
+  printf("Getting random name: %s\n", random_element(list)->name);
+  printf("Removing 'Hips Don't Lie'...\n");
+  remove_song(list, "Hips Don't Lie", "Shakira");
+  printf("\nRemoving 'American Idiot'...\n");
+  remove_song(list, "American Idiot", "Green Day");
+  printf("\nRemoving 'Something Good Can Work'...\n");
+  remove_song(list, "Something Good Can Work", "Two Door Cinema Club");
+  print_list(list);
   
-    printf("\nRemoving nonexistent name 'Hello'...\n");
-    remove_song(list, "Hello", "LOLOALDAOPWDNSBGERJNREK");
-    print_list(list);
-  
-    list = free_list(list);
-    print_list(list);
-    return 0;
+  printf("\nRemoving nonexistent name 'Hello'...\n");
+  remove_song(list, "Hello", "LOLOALDAOPWDNSBGERJNREK");
+  print_list(list);
+
+  list = free_list(list);
+  print_list(list);
+  return 0;
 }
 
--- songs.h
+++ songs.h
@@ -4,18 +4,18 @@ typedef struct song_node{
   struct song_node *next;
 } song_node;
 
-struct song_node * insert_front(struct song_node * start, char newSong[], char newArtist[]);
+song_node * insert_front(song_node * start, char newSong[], char newArtist[]);
 
-struct song_node * insert_order(struct song_node * start, char newSong[], char newArtist[]);
+song_node * insert_order(song_node * start, char newSong[], char newArtist[]);
 
-void print_list(struct song_node * start);
+void print_list(song_node * start);
 
-struct song_node * find_song(struct song_node * start, char songName[]);
+song_node * find_song(song_node * start, char songName[]);
 
-struct song_node * first_artist_song(struct song_node * start, char artistName[]);
+song_node * first_artist_song(song_node * start, char artistName[]);
 
-struct song_node * random_element(struct song_node * start);
+song_node * random_element(song_node * start);
 
-void remove_song(struct song_node * start, char songName[], char artistName[]);
+void remove_song(song_node * start, char songName[], char artistName[]);
 
-struct song_node * free_list(struct song_node * start);
+song_node * free_list(song_node * start);
